90ab3838b3f2ecfe71c0da8cf3a6cfb32aaf83f0
leavesCZY <1990724437@qq.com>
2024-05-01 23:04:07 +0800

    optimize code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # RobustWebView
 
-更多介绍请看这里：[Android WebView H5 秒开方案总结](https://juejin.cn/post/7016883220025180191)
+文章讲解：
+
+- [Android WebView H5 秒开方案总结](https://juejin.cn/post/7016883220025180191)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,17 @@
 
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
